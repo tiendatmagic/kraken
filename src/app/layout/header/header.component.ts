8804ce996decf092ac394c6b0fef23ba0a2cfa82
tiendatmagic <tiendatmagic8@gmail.com>
2024-08-20 10:33:02 +0700

    update link

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  link: string = 'https://kr11.freekraken.com/';
-  linkRegister: string = 'https://kr11.freekraken.com/register';
+  link: string = 'https://kr13.freekraken.com/';
+  linkRegister: string = 'https://kr13.freekraken.com/register';
 }
